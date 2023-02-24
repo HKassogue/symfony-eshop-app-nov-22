60ce7e32aa73c1cf8bb6dee5ad6c59b88d8573a8
ozymandias54 <bassirou718@gmail.com>
2023-02-24 12:30:17 +0000

    ajouter Reviews

--- src/Controller/DetailsController.php
+++ src/Controller/DetailsController.php
@@ -22,9 +22,28 @@ class DetailsController extends AbstractController
     #[Route('/details/{slug}', name: 'app_details')]
     public function index($slug): Response
     {
+
         $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+
+        //liste des Reviews
         $listeReview = $this->entityManager->getRepository(Review::class)->findByProduct($product);
         if (!$product) return $this->redirectToRoute('app_home');
+
+        //Enregistrement des Reviews
+        if ($_GET != null) {
+            $review = new Review();
+            $review->setRate($_GET['rate']);
+            $review->setComment($_GET['comment']);
+            $review->setName($_GET['nom']);
+            $review->setEmail($_GET['email']);
+            $review->setCreatedAt(new \DateTimeImmutable());
+            $review->setProduct($product);
+            $this->entityManager->getRepository(Review::class)->save($review, true);
+            return $this->redirectToRoute('app_details', [
+                'slug' => $slug,
+                '_fragment' => 'tab-pane-3'
+            ]);
+        }
 
         return $this->render('details/index.html.twig', [
             'product' => $product,
